14eca7c7810c92e4aeffdc38c5362e43d34fc108
Michael Wasson <mwasson@carnegielearning.com>
2025-03-28 10:17:41 +0000

    really basic rom struct

--- src/main.rs
+++ src/main.rs
@@ -26,9 +26,29 @@ fn validate_header(rom_data: &Vec<u8>) -> Result<(), io::Error> {
 		error_msg = format!("The ROM's header must meet the NES ROM specification; however, it was: {:?}", header_data);
 	}
 
+	/* TODO: This is not the correct data yet */
+	/* TODO: Would it be better to use Cow here? */
+	let rom = Rom {
+		prg_rom: (&rom_data[0..4]).to_vec(),
+		chr_ram: (&rom_data[0..4]).to_vec(),
+		byte_6_flags: rom_data[6],
+		byte_7_flags: rom_data[7],
+		prg_ram: (&rom_data[0..4]).to_vec(),
+		tv_system: rom_data[9]
+	};
+
 	if error_msg != "" {
 		return Err(Error::new(ErrorKind::InvalidData, error_msg));	
 	} else {
 		return Ok(());
 	}
+}
+
+struct Rom {
+	prg_rom: Vec<u8>,
+	chr_ram: Vec<u8>,
+	byte_6_flags: u8,
+	byte_7_flags: u8,
+	prg_ram: Vec<u8>,
+	tv_system: u8,
 }
