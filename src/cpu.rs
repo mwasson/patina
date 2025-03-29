5a16bc34d0739c08dfdb6a2d4e60656f1d504a33
Michael Wasson <mwasson@carnegielearning.com>
2025-03-29 11:02:01 +0000

    skeleton of how to do opcode parsing

--- src/cpu.rs
+++ src/cpu.rs
@@ -26,4 +26,28 @@ pub enum Instruction
     /* TODO others */
 }
 
+/*impl Instruction
+{
+	fn opcode(self) -> u8 {
+		match self {
+			LDA => 0x00,
+			_ => 0x00
+		}		
+	}
+}*/
+
+fn from_opcode(opcode: u8) -> Instruction {
+	match opcode {
+		0xa5 => Instruction::LDA, /* Zero Page, 2 bytes, 3 cycles */
+		0xa9 => Instruction::LDA, /* #Immediate, 2 bytes, 2 cycles */
+		0xa1 => Instruction::LDA, /* (Indirect,X), 2 bytes, 6 cycles */
+		0xad => Instruction::LDA, /* Absolute, 3 bytes, 4 cycles */
+		0xb1 => Instruction::LDA, /* (Indirect),Y, 2 bytes, 5/6 cycles */
+		0xb5 => Instruction::LDA, /* Zero Page,X, 2 bytes, 4 cycles */
+		0xb9 => Instruction::LDA, /* Absolute,Y, 3 bytes, 4/5 cycles */ 
+		0xbd => Instruction::LDA, /* Absolute,X, 3 bytes, 4/5 cycles */
+		_ => panic!("Unknown opcode")
+	}
+}
+
 /* TODO implementation of Instruction behavior */
