eeacc486206a0d29366a41b19270fc2db0fd1b0f
Michael Wasson <mwasson@carnegielearning.com>
2025-03-29 17:11:10 +0000

    addressing mode behaviors

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,12 +1,12 @@
 /* the state of the cpu at a given time */
 
-pub struct Cpu
+pub struct ProgramState
 {
 	accumulator: u8,
 	index_x: u8,
 	index_y: u8,
-	status: u8
-
+	status: u8,
+	memory: [u8; 1<<15]
 }
 
 pub enum Instruction
@@ -26,15 +26,65 @@ pub enum Instruction
     /* TODO others */
 }
 
-/*impl Instruction
+pub enum AddressingMode
 {
-	fn opcode(self) -> u8 {
-		match self {
-			LDA => 0x00,
-			_ => 0x00
-		}		
+	Implicit,
+	Accumulator,
+	Immediate,
+	ZeroPage,
+	ZeroPageX,
+	ZeroPageY,
+	Relative,
+	Absolute,
+	AbsoluteX,
+	AbsoluteY,
+	Indirect,
+	IndexedIndirect,
+	IndirectIndexed,
+}
+
+impl AddressingMode
+{
+	/* behavior based on: https://www.nesdev.org/obelisk-6502-guide/addressing.html */
+	fn resolve_address(self: AddressingMode, state: ProgramState, byte1:u8, byte2:u8) -> usize {
+		match self  {
+			AddressingMode::Implicit =>
+				panic!("Should never be explicitly referenced--remove?"),
+			AddressingMode::Accumulator =>
+				panic!("Should never be explicitly referenced--remove?"),
+			AddressingMode::Immediate => 
+				byte1 as usize,
+			AddressingMode::ZeroPage =>
+				zero_page_addr(byte1),
+			AddressingMode::ZeroPageX =>
+				zero_page_addr(byte1 + state.index_x),
+			AddressingMode::ZeroPageY =>
+				zero_page_addr(byte1 + state.index_y),
+			AddressingMode::Relative =>
+				panic!("Relative unimplemented for now"),
+			AddressingMode::Absolute =>
+				addr(byte1, byte2),
+			AddressingMode::AbsoluteX =>
+				addr(byte1, byte2 + state.index_x),
+			AddressingMode::AbsoluteY =>
+				addr(byte1, byte2 + state.index_y),
+			AddressingMode::Indirect => {
+                let jmp_loc = addr(byte1, byte2);
+				return addr(state.memory[jmp_loc+1], state.memory[jmp_loc]);
+			},
+            AddressingMode::IndexedIndirect => {
+				let jmp_loc = zero_page_addr(byte1 + state.index_x);
+				return addr(state.memory[jmp_loc+1], state.memory[jmp_loc]);
+			}
+			AddressingMode::IndirectIndexed => {
+				let jmp_loc = zero_page_addr(byte1 + state.index_y);
+				return addr(state.memory[jmp_loc+1], state.memory[jmp_loc]);
+			}	
+		}
 	}
-}*/
+}
+
+
 
 fn from_opcode(opcode: u8) -> Instruction {
 	match opcode {
@@ -48,6 +98,26 @@ fn from_opcode(opcode: u8) -> Instruction {
 		0xbd => Instruction::LDA, /* Absolute,X, 3 bytes, 4/5 cycles */
 		_ => panic!("Unknown opcode")
 	}
+}
+
+
+/**
+ * Converts a pair of bytes into a usize, intended to represent a 16-bit
+ * address. The first argument will be the higher order byte, the second
+ * argument the lower order. So addr(0xAB, 0xCD) returns 0xABCD.
+ */ 
+fn addr(b1:u8, b2:u8) -> usize {
+	((b1 << 7) + b2) as usize
+}
+
+/**
+ * Zero page address operations take a single-byte and result in an
+ * address on the first page of memory, which has addresses that begin
+ * with 0x00. If this is passed in 0xAB, it returns 0x00AB. In effect this
+ * is just a cast, but wrapping it as a function makes the goal clearer.
+ */
+fn zero_page_addr(b1:u8) -> usize {
+	b1 as usize
 }
 
 /* TODO implementation of Instruction behavior */
