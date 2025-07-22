7d7f008a2261cdaf6e685a0a0dc9a5e826c5c000
Michael Wasson <mwasson@carnegielearning.com>
2025-07-22 23:40:16 +0100

    indirect indexing address modes had incorrect arithmetic

--- src/cpu/addressing_mode.rs
+++ src/cpu/addressing_mode.rs
@@ -51,9 +51,9 @@ impl AddressingMode
                 state.addr_from_mem_separate_bytes(addr(byte1, byte2),
                                                    addr(byte1+1, byte2)),
             AddressingMode::IndirectX =>
-				state.addr_from_mem(zero_page_addr(byte1 + state.index_x)),
+				state.addr_from_mem(zero_page_addr(byte1.wrapping_add(state.index_x))),
 			AddressingMode::IndirectY =>
-				state.addr_from_mem(zero_page_addr(byte1 + state.index_y)),
+				state.addr_from_mem(zero_page_addr(byte1) + state.index_y as u16),
 		}
 	}
 
