2aedfb96ada62952724967ff1cc6c19846047dfe
Michael Wasson <mwasson@carnegielearning.com>
2025-07-24 16:21:01 +0100

    indirect addressing modes were completely wrong; fixed some wrapping adds

--- src/cpu/addressing_mode.rs
+++ src/cpu/addressing_mode.rs
@@ -22,7 +22,7 @@ impl AddressingMode
 {
 	/* behavior based on: https://www.nesdev.org/obelisk-6502-guide/addressing.html */
 	pub fn resolve_address(self: &AddressingMode, state: &ProgramState, byte1:u8, byte2:u8) -> u16 {
-		match self  {
+		let result = match self  {
 			AddressingMode::Implicit =>
 				panic!("Should never be explicitly referenced--remove?"),
 			AddressingMode::Accumulator =>
@@ -32,29 +32,31 @@ impl AddressingMode
 			AddressingMode::ZeroPage =>
 				zero_page_addr(byte1),
 			AddressingMode::ZeroPageX =>
-				zero_page_addr(byte1 + state.index_x),
+				zero_page_addr(byte1.wrapping_add(state.index_x)),
 			AddressingMode::ZeroPageY =>
-				zero_page_addr(byte1 + state.index_y),
+				zero_page_addr(byte1.wrapping_add(state.index_y)),
 			AddressingMode::Relative =>
 				state.program_counter
 				     .overflowing_add_signed(byte1 as i8 as i16).0,
 			AddressingMode::Absolute =>
 				addr(byte1, byte2),
 			AddressingMode::AbsoluteX =>
-				addr(byte1 + state.index_x, byte2),
+				addr(byte1, byte2) + state.index_x as u16,
 			AddressingMode::AbsoluteY =>
-				addr(byte1 + state.index_y, byte2),
+				addr(byte1, byte2) + state.index_y as u16,
 			AddressingMode::Indirect => /* only used for JMP */
 				/* this implements a bug where this mode does not
 				 * correctly handle crossing page boundaries
 				 */
                 state.addr_from_mem_separate_bytes(addr(byte1, byte2),
-                                                   addr(byte1+1, byte2)),
+                                                   addr(byte1.wrapping_add(1), byte2)),
             AddressingMode::IndirectX =>
-				state.addr_from_mem(zero_page_addr(byte1.wrapping_add(state.index_x))),
+				zero_page_addr(byte1.wrapping_add(state.index_x)),
 			AddressingMode::IndirectY =>
-				state.addr_from_mem(zero_page_addr(byte1) + state.index_y as u16),
-		}
+				state.addr_from_mem(zero_page_addr(byte1)) + state.index_y as u16,
+		};
+
+		result
 	}
 
 	/* convenience method for when you have a u16 representing an entire memory address */
