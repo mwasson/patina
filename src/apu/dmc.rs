e8569a2bdd4191a0279692aa1ac3903d8f3cf3aa
Michael Wasson <mwasson@carnegielearning.com>
2025-10-02 13:25:41 +0100

    Issue #9: untested DMC rewrite; will probably have to wait until mappers are implemented

--- src/apu/dmc.rs
+++ src/apu/dmc.rs
@@ -12,10 +12,12 @@ const NTSC_RATE_MAP : [u16; 16] = [
 pub struct DMC {
     timer: Timer,
     memory: Rc<RefCell<CoreMemory>>,
-
+    bits_remaining: u8,
     sample_buffer: Option<u8>,
-    sample_address: u8, /* offset from 0xC000 */
-    sample_length: u8, /* actual length is (sample_length * 16) + 1 bytes */
+    sample_address: u16,
+    current_address: u16,
+    sample_length: u16,
+    sample_bytes_remaining: u16,
     silence_flag: bool,
     shift_register: u8,
     volume: u8,
@@ -34,15 +36,17 @@ impl DMC {
     }
 
     pub fn new(memory: &Rc<RefCell<CoreMemory>>) -> DMC {
-        let mut timer = Timer::new();
-        timer.set_period(8);
+        let timer = Timer::new();
 
         DMC {
             timer,
             memory: memory.clone(),
+            bits_remaining: 0,
             sample_buffer: None,
             sample_address: 0,
+            current_address: 0,
             sample_length: 0,
+            sample_bytes_remaining: 0,
             silence_flag: true,
             shift_register: 0,
             volume: 0,
@@ -53,33 +57,46 @@ impl DMC {
         }
     }
     
-    pub fn tick(&mut self, apu_counter: u16) {
+    pub fn tick(&mut self, _apu_counter: u16) {
         if !self.enabled {
             return;
         }
-        if !self.silence_flag {
-            if self.shift_register & 1 == 1 {
-                if self.volume <= 125 {
-                    self.volume += 2;
-                }
-            }  else {
-                if self.volume >= 2 {
+
+        if self.sample_buffer.is_none() && self.sample_bytes_remaining > 0 {
+            self.sample_buffer = Some(self.memory.borrow().read(self.current_address));
+            if self.current_address == 0xffff {
+                self.current_address = 0x8000;
+            } else {
+                self.current_address += 1;
+            }
+            self.sample_bytes_remaining -= 1;
+            if self.sample_bytes_remaining == 0 && self.loop_flag {
+                self.current_address = self.sample_address;
+                self.sample_bytes_remaining = self.sample_length;
+            }
+            /* TODO IRQ interrupt */
+        }
+
+        if self.timer.clock() {
+            if !self.silence_flag {
+                if self.shift_register & 1 != 0 {
+                    if self.volume < 125 {
+                        self.volume += 2;
+                    }
+                } else if self.volume > 1 {
                     self.volume -= 2;
                 }
             }
-        }
-        /* TODO clock shift register??? */
-
-        if self.timer.clock() {
-            match self.sample_buffer {
-                Some(sample) => {
-                    self.silence_flag = false;
-                    self.shift_register = sample;
-                    self.sample_buffer = None;
-                    /* TODO read memory here? */
-                }
-                None => {
+            /* TODO clock shift register */
+            self.shift_register >>= 1;
+            self.bits_remaining -= 1;
+            if self.bits_remaining == 0 {
+                if self.sample_buffer.is_none() {
                     self.silence_flag = true;
+                } else {
+                    self.silence_flag = false;
+                    self.shift_register = self.sample_buffer.unwrap();
+                    self.sample_buffer = None;
                 }
             }
         }
@@ -104,22 +121,28 @@ impl MemoryListener for DMC {
         memory.read(address)
     }
 
-    fn write(&mut self, memory: &CoreMemory, address: u16, value: u8) {
+    fn write(&mut self, _memory: &CoreMemory, address: u16, value: u8) {
         match address {
             0x4010 => {
                 self.irq_enabled = value & 0x80 != 0; /* TODO: clear interrupt flag when cleared */
                 self.loop_flag = value & 0x40 != 0;
-                self.rate_index = NTSC_RATE_MAP[(value & 0x0f) as usize];
+                /* map from number of CPU cycles to number of APU cycles */
+                self.rate_index = NTSC_RATE_MAP[(value & 0x0f) as usize] >> 2;
+                self.timer.set_period(self.rate_index);
             }
             0x4011 => {
                 /* writes value directly, but usually load a sample instead */
                 self.volume = value;
             }
             0x4012 => {
-                self.sample_address = value;
+                /* memory value is offset from 0xc000, aligned to 64 byte chunks */
+                self.sample_address = 0xc000 | ((value as u16) << 6);
+                self.current_address = self.sample_address;
             }
             0x4013 => {
-                self.sample_length = value;
+                /* actual length is (sample_length * 16) + 1 bytes */
+                self.sample_length = ((value as u16) << 4) | 1;
+                self.sample_bytes_remaining = self.sample_length;
             }
             _ => unreachable!()
         }
